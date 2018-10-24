4ef966964d49efb4e6d0451355c083edf7f87a58
Abukodonosor <stojiljkovic.uros@gmail.com>
2018-10-24 12:28:58 +0200

    transpiled tsc

--- dist/models/UserModel.js
+++ dist/models/UserModel.js
@@ -3,11 +3,9 @@ var __extends = (this && this.__extends) || (function () {
     var extendStatics = function (d, b) {
         extendStatics = Object.setPrototypeOf ||
             ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b)
-                if (b.hasOwnProperty(p))
-                    d[p] = b[p]; };
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
         return extendStatics(d, b);
-    };
+    }
     return function (d, b) {
         extendStatics(d, b);
         function __() { this.constructor = d; }
@@ -15,16 +13,17 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 Object.defineProperty(exports, "__esModule", { value: true });
-var db_1 = require("./db");
-var User = /** @class */ (function (_super) {
-    __extends(User, _super);
-    function User(username, password) {
+var DB_1 = require("./DB");
+var Users = /** @class */ (function (_super) {
+    __extends(Users, _super);
+    function Users(email, password, username) {
         var _this = _super.call(this) || this;
-        _this.username = username;
+        _this.email = email;
         _this.password = password;
+        _this.username = username;
         return _this;
     }
-    User.fillAll = function () {
+    Users.save = function () {
         var _this = this;
         return new Promise(function (resolve) {
             _this.conn.query("SELECT * FROM user", [], function (err, rows) {
@@ -32,6 +31,6 @@ var User = /** @class */ (function (_super) {
             });
         });
     };
-    return User;
-}(db_1.DB));
-exports.User = User;
+    return Users;
+}(DB_1.DB));
+exports.Users = Users;
--- dist/typescript/built/config/configDB.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.config = {
-    host: 'localhost',
-    user: 'root',
-    password: 'qweqwe123',
-    database: 'freak_out'
-};
